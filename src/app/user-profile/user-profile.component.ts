754b16f9ae37a032d29d12767c1126afa7ebcd09
Jess-Wild <j.victorion.wcs@gmail.com>
2022-10-25 23:18:18 +0200

    Update Quest 02 End

---
+++ src/app/user-profile/user-profile.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.css']
+})
+export class UserProfileComponent implements OnInit {
+  user = {
+    name: "Doe",
+    firstName: "John",
+    age: 25,
+    quote: "Salut !",
+    photo: 'https://randomuser.me/api/portraits/lego/2.jpg'
+  };
+
+  display = false;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
